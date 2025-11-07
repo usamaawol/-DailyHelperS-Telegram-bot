c16e5f37b339510aedb089b396207423c6ce1322
USAMA AWOL <usamaawol0@gmail.com>
2025-11-07 23:43:38 +0300

    Remove sensitive/test files

--- test_env.py
+++
@@ -1,8 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-# Load .env from current working directory
-load_dotenv(".env")
-
-print(os.getenv("OPENWEATHER_API_KEY"))   # Should print your key
-print(os.getenv("TELEGRAM_BOT_TOKEN"))    # Should print your bot token
